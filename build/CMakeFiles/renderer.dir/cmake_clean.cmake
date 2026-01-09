f524d6883629f56293cf6684a4e89aea55ee0d35
yash <yashchaurasia630@gmail.com>
2026-01-09 15:49:18 +0530

    initial commit

---
+++ build/CMakeFiles/renderer.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/renderer.dir/dependencies/lib/glad.c.obj"
+  "CMakeFiles/renderer.dir/dependencies/lib/glad.c.obj.d"
+  "CMakeFiles/renderer.dir/src/main.cpp.obj"
+  "CMakeFiles/renderer.dir/src/main.cpp.obj.d"
+  "librenderer.dll.a"
+  "renderer.exe"
+  "renderer.exe.manifest"
+  "renderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+
+const unsigned int SCR_WIDTH = 800, SCR_HEIGHT = 600;
+
+void framebufferSizeCallback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int main()
+{
+  std::cout << "hello" << std::endl;
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifndef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+  GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Renderer", NULL, NULL);
+  if (window == NULL)
+  {
+    std::cout << "Failed to create a GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to load OpenGL function pointers" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+  glEnable(GL_DEPTH_TEST);
+  glClearColor(0.5f, 0.3f, 0.2f, 1.0f);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    processInput(window);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+
+  glfwTerminate();
+  return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
